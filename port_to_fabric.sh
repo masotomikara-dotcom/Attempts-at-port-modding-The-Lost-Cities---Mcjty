473be8be34ffb3f2779b8b9e4dddc4b53fc647c5
masotomikara-dotcom <masotomikara@gmail.com>
2026-05-01 15:29:29 +0700

    Major mapping switch to Mojmap and heavy Forge class patching

--- port_to_fabric.sh
+++ port_to_fabric.sh
@@ -27,11 +27,10 @@ repositories {
 
 dependencies {
     minecraft 'com.mojang:minecraft:1.20.1'
-    mappings 'net.fabricmc:yarn:1.20.1+build.10:v2'
+    mappings loom.officialMojangMappings()
     modImplementation 'net.fabricmc:fabric-loader:0.15.11'
     modImplementation 'net.fabricmc.fabric-api:fabric-api:0.92.2+1.20.1'
     modImplementation 'dev.architectury:architectury-fabric:9.2.14'
-    
     implementation fileTree(dir: 'libs', include: ['*.jar'])
 }
 
@@ -42,7 +41,16 @@ EOM
 
 find src -type f -name "*.java" -exec sed -i 's/net.minecraftforge.fml.common.Mod/dev.architectury.injectables.annotations.ExpectPlatform/g' {} +
 find src -type f -name "*.java" -exec sed -i 's/net.minecraftforge.eventbus.api.SubscribeEvent/dev.architectury.event.EventResult/g' {} +
-find src -type f -name "*.java" -exec sed -i 's/net.minecraftforge.common.MinecraftForge/dev.architectury.platform.Platform/g' {} +
+find src -type f -name "*.java" -exec sed -i 's/net.minecraftforge.api.distmarker.Dist/net.fabricmc.api.EnvType/g' {} +
+find src -type f -name "*.java" -exec sed -i 's/net.minecraftforge.api.distmarker.OnlyIn/net.fabricmc.api.Environment/g' {} +
+find src -type f -name "*.java" -exec sed -i 's/Dist.CLIENT/EnvType.CLIENT/g' {} +
+find src -type f -name "*.java" -exec sed -i 's/Dist.DEDICATED_SERVER/EnvType.SERVER/g' {} +
+find src -type f -name "*.java" -exec sed -i 's/net.minecraftforge.fml.loading.FMLPaths/net.fabricmc.loader.api.FabricLoader/g' {} +
+find src -type f -name "*.java" -exec sed -i 's/FMLPaths.CONFIGDIR.get()/FabricLoader.getInstance().getConfigDir()/g' {} +
+find src -type f -name "*.java" -exec sed -i 's/net.minecraftforge.registries.ForgeRegistries/net.minecraft.core.registries.BuiltInRegistries/g' {} +
+find src -type f -name "*.java" -exec sed -i 's/net.minecraftforge.registries.DeferredRegister/dev.architectury.registry.registries.DeferredRegister/g' {} +
+find src -type f -name "*.java" -exec sed -i 's/net.minecraftforge.registries.RegistryObject/dev.architectury.registry.registries.RegistrySupplier/g' {} +
+find src -type f -name "*.java" -exec sed -i 's/import net.minecraftforge.*;//g' {} +
 
 mkdir -p src/main/resources
 cat <<EOM > src/main/resources/fabric.mod.json
