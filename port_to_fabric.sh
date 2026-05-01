194a5ad74e0d2df7ed97814e15c75efe912aac6e
masotomikara-dotcom <masotomikara@gmail.com>
2026-05-01 14:51:47 +0700

    Update port script with English comments and fixed download link

--- port_to_fabric.sh
+++ port_to_fabric.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-# 1. Force Upgrade Gradle Wrapper Version
+
+# 1. Force Upgrade Gradle Wrapper to version 8.7
+echo "Upgrading Gradle Wrapper..."
 sed -i 's/gradle-7.3-bin.zip/gradle-8.7-bin.zip/g' gradle/wrapper/gradle-wrapper.properties
 
-# 2. Setup Gradle Environment
+# 2. Setup settings.gradle
+echo "Configuring settings.gradle..."
 cat <<EOM > settings.gradle
 pluginManagement {
     repositories {
@@ -13,7 +16,39 @@ pluginManagement {
 rootProject.name = 'lostcities'
 EOM
 
-# 3. Fabric Metadata
+# 3. Setup build.gradle for Fabric port
+echo "Configuring build.gradle..."
+cat <<EOM > build.gradle
+plugins {
+    id 'fabric-loom' version '1.6-SNAPSHOT'
+}
+version = '1.20.1-1.0.0'
+group = 'mcjty.lostcities'
+
+repositories {
+    maven { url "https://maven.architectury.dev/" }
+    maven { url "https://www.cursemaven.com" }
+    flatDir { dirs 'libs' }
+}
+
+dependencies {
+    minecraft 'com.mojang:minecraft:1.20.1'
+    mappings 'net.fabricmc:yarn:1.20.1+build.10:v2'
+    modImplementation 'net.fabricmc:fabric-loader:0.15.11'
+    modImplementation 'net.fabricmc.fabric-api:fabric-api:0.92.2+1.20.1'
+    modImplementation 'dev.architectury:architectury-fabric:9.2.14'
+    
+    // Using local jar from libs folder to avoid maven resolution issues
+    implementation fileTree(dir: 'libs', include: ['*.jar'])
+}
+
+tasks.withType(JavaCompile).configureEach {
+    it.options.release = 17
+}
+EOM
+
+# 4. Generate Fabric Metadata
+echo "Generating fabric.mod.json..."
 mkdir -p src/main/resources
 cat <<EOM > src/main/resources/fabric.mod.json
 {
@@ -35,71 +70,33 @@ cat <<EOM > src/main/resources/fabric.mod.json
 }
 EOM
 
-# 4. Global Code Patching
-find src -type f -name "*.java" -exec sed -i 's/net.minecraftforge.fml.common.Mod/dev.architectury.injectables.annotations.ExpectPlatform/g' {} +
-find src -type f -name "*.java" -exec sed -i 's/net.minecraftforge.eventbus.api.SubscribeEvent/dev.architectury.event.EventResult/g' {} +
-find src -type f -name "*.java" -exec sed -i 's/net.minecraftforge.common.MinecraftForge/dev.architectury.platform.Platform/g' {} +
-find src -type f -name "*.java" -exec sed -i 's/net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext/dev.architectury.utils.Env/g' {} +
-find src -type f -name "*.java" -exec sed -i 's/net.minecraftforge.api.distmarker.Dist/net.fabricmc.api.EnvType/g' {} +
-find src -type f -name "*.java" -exec sed -i 's/net.minecraftforge.api.distmarker.OnlyIn/net.fabricmc.api.Environment/g' {} +
-
-# 5. Create Entrypoint
+# 5. Create Fabric Entrypoint
+echo "Creating Fabric Entrypoint..."
+mkdir -p src/main/java/mcjty/lostcities
 cat <<EOM > src/main/java/mcjty/lostcities/FabricEntrypoint.java
 package mcjty.lostcities;
 import net.fabricmc.api.ModInitializer;
 public class FabricEntrypoint implements ModInitializer {
     @Override
     public void onInitialize() {
+        // LostCities main class initialization
         new LostCities();
     }
 }
 EOM
 
-# 6. Build Configuration (Use Modrinth Maven)
-cat <<EOM > build.gradle
-plugins {
-    id 'fabric-loom' version '1.6-SNAPSHOT'
-}
-version = '1.20.1-1.0.0'
-group = 'mcjty.lostcities'
+# 6. Manually download McJtyLib (Fixed version for 1.20.1)
+echo "Downloading McJtyLib dependency..."
+mkdir -p libs
+curl -L -s -o libs/mcjtylib.jar "https://www.cursemaven.com/curse/maven/mcjtylib-233105/4615378/mcjtylib-233105-4615378.jar"
 
-repositories {
-    maven { url "https://maven.architectury.dev/" }
-    maven { url "https://api.modrinth.com/maven" }
-}
+# Check if file download was successful
+if [ ! -s libs/mcjtylib.jar ]; then
+    echo "ERROR: McJtyLib download failed or file is empty!"
+    exit 1
+fi
 
-sourceSets {
-    main {
-        java {
-            srcDirs += ['src/api/java', 'src/generated/resources']
-        }
-    }
-}
-
-dependencies {
-    minecraft 'com.mojang:minecraft:1.20.1'
-    mappings 'net.fabricmc:yarn:1.20.1+build.10:v2'
-    modImplementation 'net.fabricmc:fabric-loader:0.15.11'
-    modImplementation 'net.fabricmc.fabric-api:fabric-api:0.92.2+1.20.1'
-    modImplementation 'dev.architectury:architectury-fabric:9.2.14'
-    
-    // McJtyLib for 1.20.1 from Modrinth
-    modImplementation "maven.modrinth:mcjtylib:v8.0.3"
-}
-
-tasks.withType(JavaCompile).configureEach {
-    it.options.release = 17
-}
-EOM
-
-cat <<EOM > gradle.properties
-org.gradle.jvmargs=-Xmx2G
-org.gradle.parallel=true
-EOM
-
-# 7. Clean up old failed jars
-rm -rf libs/
-
-# 8. Run Build
+# 7. Execute Build and export log
+echo "Starting build process..."
 chmod +x gradlew
-./gradlew clean build --stacktrace
+./gradlew clean build --stacktrace 2>&1 | tee build_log.txt
