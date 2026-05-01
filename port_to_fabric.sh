63a226e7d228d9a6eb69ef053f295956033c1997
masotomikara-dotcom <masotomikara@gmail.com>
2026-05-01 15:33:10 +0700

    Update script with English documentation and Phase 2 patches

--- port_to_fabric.sh
+++ port_to_fabric.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# Upgrade Gradle to 8.7 for Java 17/21 compatibility
 sed -i 's/gradle-7.3-bin.zip/gradle-8.7-bin.zip/g' gradle/wrapper/gradle-wrapper.properties
 
+# Configure settings.gradle
 cat <<EOM > settings.gradle
 pluginManagement {
     repositories {
@@ -12,6 +14,7 @@ pluginManagement {
 rootProject.name = 'lostcities'
 EOM
 
+# Configure build.gradle with Mojmap and Architectury
 cat <<EOM > build.gradle
 plugins {
     id 'fabric-loom' version '1.6-SNAPSHOT'
@@ -39,19 +42,33 @@ tasks.withType(JavaCompile).configureEach {
 }
 EOM
 
-find src -type f -name "*.java" -exec sed -i 's/net.minecraftforge.fml.common.Mod/dev.architectury.injectables.annotations.ExpectPlatform/g' {} +
-find src -type f -name "*.java" -exec sed -i 's/net.minecraftforge.eventbus.api.SubscribeEvent/dev.architectury.event.EventResult/g' {} +
-find src -type f -name "*.java" -exec sed -i 's/net.minecraftforge.api.distmarker.Dist/net.fabricmc.api.EnvType/g' {} +
-find src -type f -name "*.java" -exec sed -i 's/net.minecraftforge.api.distmarker.OnlyIn/net.fabricmc.api.Environment/g' {} +
-find src -type f -name "*.java" -exec sed -i 's/Dist.CLIENT/EnvType.CLIENT/g' {} +
-find src -type f -name "*.java" -exec sed -i 's/Dist.DEDICATED_SERVER/EnvType.SERVER/g' {} +
-find src -type f -name "*.java" -exec sed -i 's/net.minecraftforge.fml.loading.FMLPaths/net.fabricmc.loader.api.FabricLoader/g' {} +
-find src -type f -name "*.java" -exec sed -i 's/FMLPaths.CONFIGDIR.get()/FabricLoader.getInstance().getConfigDir()/g' {} +
-find src -type f -name "*.java" -exec sed -i 's/net.minecraftforge.registries.ForgeRegistries/net.minecraft.core.registries.BuiltInRegistries/g' {} +
-find src -type f -name "*.java" -exec sed -i 's/net.minecraftforge.registries.DeferredRegister/dev.architectury.registry.registries.DeferredRegister/g' {} +
-find src -type f -name "*.java" -exec sed -i 's/net.minecraftforge.registries.RegistryObject/dev.architectury.registry.registries.RegistrySupplier/g' {} +
-find src -type f -name "*.java" -exec sed -i 's/import net.minecraftforge.*;//g' {} +
+# --- Start Patching Phase ---
 
+# Remove Forge-specific DataGen (Incompatible with Fabric)
+rm -rf src/main/java/mcjty/lostcities/datagen
+
+# Remove Forge @Mod annotations and event subscribers
+find src -type f -name "*.java" -exec sed -i 's/@Mod(.*)//g' {} +
+find src -type f -name "*.java" -exec sed -i 's/@EventBusSubscriber.*//g' {} +
+find src -type f -name "*.java" -exec sed -i 's/import net.minecraftforge.fml.common.Mod;//g' {} +
+
+# Replace legacy javax annotations with JetBrains standard
+find src -type f -name "*.java" -exec sed -i 's/import javax.annotation.Nonnull;/import org.jetbrains.annotations.NotNull;/g' {} +
+find src -type f -name "*.java" -exec sed -i 's/import javax.annotation.Nullable;/import org.jetbrains.annotations.Nullable;/g' {} +
+find src -type f -name "*.java" -exec sed -i 's/@Nonnull/@NotNull/g' {} +
+
+# Redirect Forge side-loading logic to Architectury Platform
+find src -type f -name "*.java" -exec sed -i 's/DistExecutor.unsafeRunWhenOn/if (dev.architectury.platform.Platform.getEnv().isClient())/g' {} +
+
+# Fix Registry system (Forge DeferredRegister -> Architectury)
+find src -type f -name "*.java" -exec sed -i 's/DeferredRegister.create/DeferredRegister.create(LostCities.MODID, /g' {} +
+find src -type f -name "*.java" -exec sed -i 's/.register(IEventBus.*)//g' {} +
+
+# Clean up remaining Forge lifecycle imports
+find src -type f -name "*.java" -exec sed -i 's/import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;//g' {} +
+find src -type f -name "*.java" -exec sed -i 's/import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;//g' {} +
+
+# Prepare Fabric metadata
 mkdir -p src/main/resources
 cat <<EOM > src/main/resources/fabric.mod.json
 {
@@ -73,6 +90,7 @@ cat <<EOM > src/main/resources/fabric.mod.json
 }
 EOM
 
+# Create the Fabric-compatible main entry point
 mkdir -p src/main/java/mcjty/lostcities
 cat <<EOM > src/main/java/mcjty/lostcities/FabricEntrypoint.java
 package mcjty.lostcities;
@@ -85,5 +103,6 @@ public class FabricEntrypoint implements ModInitializer {
 }
 EOM
 
+# Finalize and Build
 chmod +x gradlew
 ./gradlew clean build 2>&1 | tee build_log.txt
