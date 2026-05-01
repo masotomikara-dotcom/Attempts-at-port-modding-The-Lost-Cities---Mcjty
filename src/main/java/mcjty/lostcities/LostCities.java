c6b5a2319e47b9c096c113c39e9ec1f537212fcb
masotomikara-dotcom <masotomikara@gmail.com>
2026-05-01 16:20:46 +0700

    Final cleanup of LostCities.java structure

--- src/main/java/mcjty/lostcities/LostCities.java
+++ src/main/java/mcjty/lostcities/LostCities.java
@@ -1,78 +1,16 @@
 package mcjty.lostcities;
 
-import mcjty.lostcities.api.ILostCities;
-import mcjty.lostcities.api.ILostCitiesPre;
-import mcjty.lostcities.setup.*;
-import net.fabricmc.api.EnvType;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.ModLoadingContext;
-import dev.architectury.injectables.annotations.ExpectPlatform;
-import net.minecraftforge.fml.config.ModConfig;
-import dev.architectury.utils.Env;
-import net.minecraftforge.fml.loading.FMLEnvironment;
-import net.minecraftforge.fml.loading.FMLPaths;
+import mcjty.lostcities.setup.Registration;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.File;
-import java.nio.file.Path;
-import java.util.function.Function;
-import java.util.function.Supplier;
-
-
-
 public class LostCities {
+
     public static final String MODID = "lostcities";
-
-    public static final Logger LOGGER = LogManager.getLogger(LostCities.MODID);
-
-    public static final ModSetup setup = new ModSetup();
-    public static LostCities instance;
-    public static final LostCitiesImp lostCitiesImp = new LostCitiesImp();
+    public static final Logger LOGGER = LogManager.getLogger();
+    public static LostCitiesImp lostCitiesImp = new LostCitiesImp();
 
     public LostCities() {
-        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-        Dist dist = FMLEnvironment.dist;
-
-        instance = this;
-
-        Registration.init(bus);
-        CustomRegistries.init(bus);
-
-        Path configPath = FMLPaths.CONFIGDIR.get();
-        File dir = new File(configPath + File.separator + "lostcities");
-        dir.mkdirs();
-
-        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, Config.CLIENT_CONFIG, "lostcities/client.toml");
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.COMMON_CONFIG, "lostcities/common.toml");
-        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, Config.SERVER_CONFIG);
-
-        setup.preInit();
-        bus.addListener(setup::init);
-        bus.addListener(this::processIMC);
-        bus.addListener(this::onConstructModEvent);
-        bus.addListener(CustomRegistries::onDataPackRegistry);
-
-        if (dist.isClient()) {
-            bus.addListener(ClientSetup::init);
-        }
-    }
-
-    public static Logger getLogger() {
-        return LOGGER;
-    }
-
-        event.enqueueWork(() -> {
-            event.getIMCStream(ILostCities.GET_LOST_CITIES_PRE::equals).forEach(message -> {
-                Supplier<Function<ILostCitiesPre, Void>> supplier = message.getMessageSupplier();
-                supplier.get().apply(new LostCitiesPreImp());
-            });
-        });
-    }
-
-        event.getIMCStream(ILostCities.GET_LOST_CITIES::equals).forEach(message -> {
-            Supplier<Function<ILostCities, Void>> supplier = message.getMessageSupplier();
-            supplier.get().apply(lostCitiesImp);
-        });
+        Registration.init();
     }
 }
